2b6a75efe1379db4c96dda357fd027ebc03e2ed2
Vladimír Žák <vladoyak@gmail.com>
2020-05-10 10:39:51 +0200

    Initial commit.

---
+++ Vladoyak.Tourister/Vladoyak.TouristerConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Vladoyak.TouristerConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
