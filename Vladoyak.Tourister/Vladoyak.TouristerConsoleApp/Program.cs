6ea6a5c5fb5f22f81f88688e44405e5da79b017f
Vladimír Žák <vladoyak@gmail.com>
2020-05-10 16:40:46 +0200

    Domain model.

--- Vladoyak.Tourister/Vladoyak.TouristerConsoleApp/Program.cs
+++ Vladoyak.Tourister/Vladoyak.TouristerConsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Vladoyak.AllInOneApp;
 
 namespace Vladoyak.TouristerConsoleApp
 {
@@ -6,8 +7,44 @@ namespace Vladoyak.TouristerConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine();
+            var app = new TouristerApp();
+            app.Init();
+
+            // write all trails
+            Console.WriteLine("TRAILS:");
+            foreach (var trail in app.Trails.Values)
+            {
+                Console.WriteLine($"  {trail}");
+                foreach (var point in trail.TrailPoints)
+                {
+                    Console.WriteLine($"    {point}");
+                }
+            }
+
+            // write all signposts
+            Console.WriteLine("SIGNPOSTS:");
+            foreach (var signpost in app.Signposts.Values)
+            {
+                Console.WriteLine($"  {signpost}");
+                foreach (var directionSign in signpost.DirectionSigns)
+                {
+                    foreach (var direction in directionSign.Directions)
+                    {
+                        Console.WriteLine($"    {direction}");
+                    }
+                }
+            }
+
+            // write all trips
+            Console.WriteLine("TRIPS:");
+            foreach (var trip in app.Trips.Values)
+            {
+                Console.WriteLine($"  {trip}");
+                foreach (var trailPointVisit in trip.TrailPointVisits)
+                {
+                    Console.WriteLine($"    {trailPointVisit}");
+                }
+            }
         }
     }
 }
